0465ed719a3e1d16db44cedd2eabc2a7933b4d3b
Trần Văn Mạnh <86815257+tranvanmanh9325@users.noreply.github.com>
2024-02-10 10:24:50 +0700

    Update intro.js

--- intro.js
+++ intro.js
@@ -45,10 +45,3 @@ closeSlider3.addEventListener('click', function(){
 
 
 // ------audio------------
- var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-  }
