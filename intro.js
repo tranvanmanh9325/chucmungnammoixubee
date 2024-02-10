3b4ed773eabf1422d83f0479ebeadc64e1b49ad7
Trần Văn Mạnh <86815257+tranvanmanh9325@users.noreply.github.com>
2024-02-10 11:05:06 +0700

    Update intro.js

--- intro.js
+++ intro.js
@@ -45,10 +45,3 @@ closeSlider3.addEventListener('click', function(){
 
 
 // ------audio------------
-var buttonSong = document.querySelector('.button')
-var mySong = document.getElementById("song")
-buttonSong.onclick= function(){
-    if(mySong.paused){
-        mySong.play()
-    }
-}
